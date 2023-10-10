7db5a903e14c45c241d167338459501bde7a3096
Thierno Diallo <63030553+Thierno-M-Diallo@users.noreply.github.com>
2023-10-09 23:22:32 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,52 +1,38 @@
-#import tkinter as tk
-#from tkinter import messagebox
-
-# Function to display a message when the button is clicked
-#def display_message():
-#    messagebox.showinfo("Message", "Button Clicked!")
-
-# Create the main application window
-#root = tk.Tk()
-#root.title("Simple GUI")
-
-# Create a button
-#button = tk.Button(root, text="Click me", command=display_message)
-
-# Pack the button to add it to the window
-#button.pack(pady=20)
-
-# Run the main event loop
-#root.mainloop()
 import tkinter as tk
+from tkinter import ttk
 
-# Function to display race results
-def display_race_results():
-    # Implement race results display logic here
-    print("Displaying race results...")
-
-# Function to display driver standings
-def display_driver_standings():
-    # Implement driver standings display logic here
-    print("Displaying driver standings...")
-
-# Function to display tire strategy visualizations
-def display_tire_strategy():
-    # Implement tire strategy display logic here
-    print("Displaying tire strategy...")
+# Function to handle the selection from the burger menu
+def menu_selected(event):
+    selected_menu_item = menu_var.get()
+    print("Selected menu item:", selected_menu_item)
 
 # Create the main application window
 root = tk.Tk()
-root.title("F1 Race Database")
+root.title("F1 Racing GUI")
 
-# Create buttons for different functionalities
-results_button = tk.Button(root, text="Display Race Results", command=display_race_results)
-standings_button = tk.Button(root, text="Display Driver Standings", command=display_driver_standings)
-tire_strategy_button = tk.Button(root, text="Display Tire Strategy", command=display_tire_strategy)
+# Create a menu bar
+menu_bar = tk.Menu(root)
 
-# Pack the buttons to add them to the window
-results_button.pack(pady=10)
-standings_button.pack(pady=10)
-tire_strategy_button.pack(pady=10)
+# Create a menu item
+menu = tk.Menu(menu_bar, tearoff=0)
+menu.add_command(label="Display Race Results")
+menu.add_command(label="Display Driver Standings")
+menu.add_command(label="Display Tire Strategy")
+
+# Add the menu to the menu bar
+menu_bar.add_cascade(label="Burger Menu", menu=menu)
+
+# Configure the root to use the menu bar
+root.config(menu=menu_bar)
+
+# Create a label
+label = ttk.Label(root, text="Welcome to the F1 Racing GUI!")
+label.pack(pady=20)
+
+# Create a Combobox with F1-related options
+options = ["Select an option", "Main Screen", "Live Screen", "Team Points Screen", "Penalty Screen", "Strategy Screen"]
+combo = ttk.Combobox(root, values=options)
+combo.pack(pady=10)
 
 # Run the main event loop
 root.mainloop()
