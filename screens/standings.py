7e0d354bba2875ae7add93b0e12f116fac13437e
Faddi Dabain <dottycr02@gmail.com>
2023-11-17 20:31:14 -0500

    Fixed the season menu bug, but has unwanted effects.

--- screens/standings.py
+++ screens/standings.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from datetime import date
-
 
 class Standings(ctk.CTkFrame):
     def __init__(self, parent, controller, db):
@@ -8,7 +9,8 @@ class Standings(ctk.CTkFrame):
         self.scrollable_frame = None
         self.controller = controller
         self.data = db
-        self.load_data("drivers")
+        self.table = 'drivers'
+        self.load_data(self.table)
         self.setup_ui()
 
     def setup_ui(self):
@@ -25,14 +27,72 @@ class Standings(ctk.CTkFrame):
         self.fill()
 
     def load_data(self, table, season=date.today().year):
-        query = f"SELECT name, points FROM {table} WHERE season = {season} ORDER BY points DESC"
-        self.data.execute(query)
+        if table == 'drivers':
+            query = f"SELECT name, points, team FROM drivers WHERE season = {season} ORDER BY points DESC"
+            self.data.execute(query)
+        elif table == 'teams':
+            query = f"SELECT name, points FROM teams WHERE season = {season} ORDER BY points DESC"
+            self.data.execute(query)
+        else:
+            print("Error in tabel selected")
+
+        self.table = table
 
     def fill(self):
         for widget in self.scrollable_frame.winfo_children():
             widget.destroy()
 
-        for index, item in enumerate(self.data):
+        fetched = self.data.fetchall()
+
+        # Initialize lists for the pie chart data
+        labels = []
+        sizes = []
+        team_colors_list = []
+        colors = {
+            'Red Bull': '#1E41FF',
+            'Mercedes': '#00D2BE',
+            'Ferrari': '#DC0000',
+            'Aston Martin': '#006F62',
+            'McLaren': '#FF8700',
+            'Alpine': '#0090FF',
+            'Williams': '#005AFF',
+            'Haas': '#FFFFFF',
+            'Alfa Romeo': '#900000',
+            'AlphaTauri': '#2B4562'
+        }
+
+        # Fetch data and fill the labels and sizes lists
+        if self.table == 'drivers':
+            for name, points, team in fetched:
+                labels.append(name)
+                sizes.append(points)
+                team_colors_list.append(colors.get(team))
+        else:
+            for name, points in fetched:
+                labels.append(name)
+                sizes.append(points)
+                team_colors_list.append(colors.get(name))
+
+        # Create a figure for the plot
+        fig, ax = plt.subplots()
+
+        # Manually set the background color
+        background_color = "#2e2e2e"  # Adjust this to match your theme color
+        fig.patch.set_facecolor(background_color)
+        ax.set_facecolor(background_color)
+
+        # Generate a list of colors for the teams
+        team_colors = [colors.get(team, "#FFFFFF") for team in labels]
+
+        ax.pie(sizes, labels=labels, autopct='%1.1f%%', colors=team_colors_list)
+
+        # Create a canvas and add the figure to it
+        canvas = FigureCanvasTkAgg(fig, master=self.scrollable_frame)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.grid(row=0, column=0, columnspan=3)
+
+        # Add other widgets below the pie chart
+        for index, item in enumerate(fetched):
             leaderT = item[0]
             ptsT = item[1]
 
@@ -40,10 +100,10 @@ class Standings(ctk.CTkFrame):
             ptsT_formatted = str(int(ptsT)) if ptsT.is_integer() else str(ptsT)
 
             resultL = ctk.CTkLabel(self.scrollable_frame, text=index + 1, font=("Lucidia Sans", 17))
-            resultL.grid(row=index, column=0, sticky="nw", padx=20, pady=10)
+            resultL.grid(row=index + 1, column=0, sticky="nw", padx=20, pady=10)
 
             leaderL = ctk.CTkLabel(self.scrollable_frame, text=leaderT, font=("Lucidia Sans", 17))
-            leaderL.grid(row=index, column=1, sticky="nw", padx=20, pady=10)
+            leaderL.grid(row=index + 1, column=1, sticky="nw", padx=20, pady=10)
 
             ptsL = ctk.CTkLabel(self.scrollable_frame, text=ptsT_formatted, font=("Lucidia Sans", 17))
-            ptsL.grid(row=index, column=2, sticky="e", padx=20, pady=10)
+            ptsL.grid(row=index + 1, column=2, sticky="e", padx=20, pady=10)
