9ff4df5eca53978f5cc48d79e607b87382ce3205
Faddi Azzam Dabain <96977807+FaddiADabain@users.noreply.github.com>
2023-11-21 13:15:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,9 @@ class MainApplication(ctk.CTk):
         self.season_menu.set("2023")
         self.season_menu.pack(side="left", padx=10)
 
+        # Initialize a list to store race values
+        self.race_values = []
+
         # Create the race option menu inside the frame
         self.race_menu = ctk.CTkOptionMenu(self.options_frame,
                                            command=self.race_menu_callback, button_color="grey", fg_color="grey",
@@ -101,31 +104,31 @@ class MainApplication(ctk.CTk):
         # Show the selected frame
         self.show_frame(selected_value)
 
-        # Check if the selected frame is 'Standings'
         if selected_value == "Standings":
-            # Configure the race menu for the 'Standings' frame
+        # Configure the race menu for the 'Standings' frame
             self.race_menu.configure(values=("Drivers' Championship", "Constructors' Championship"))
             self.race_menu.set("Drivers' Championship")
             self.standingsF.load_data(table="Drivers' Championship", season=self.season_menu.get())
             self.standingsF.fill()
-
         else:
-            # Configure the race menu for other frames
-            self.race_menu_vals(self.season_menu.get())
-            if self.last_selected_race is not None and self.last_selected_race in self.race_menu.values:
+        # Update the race menu based on the last selected race
+            if self.last_selected_race in self.race_values:
                 self.race_menu.set(self.last_selected_race)
             else:
-                self.last_selected_race = self.race_menu.values[0]
+                self.last_selected_race = self.race_values[0]
                 self.race_menu.set(self.last_selected_race)
 
             self.update_frames(selected_value, self.race_menu.get(), self.season_menu.get())
 
     def season_menu_callback(self, selected_value):
-        if selected_value != self.last_selected_season:
-            self.last_selected_season = selected_value
+        if self.segmented_buttons.get() != "Standings":
+            # Update the race menu values based on the selected season
             self.race_menu_vals(selected_value)
-            self.last_selected_race = self.race_menu.values[0]
-            self.race_menu.set(self.last_selected_race)
+        
+            # Set the race menu to the first race of the season
+            if self.race_values:
+                self.last_selected_race = self.race_values[0]
+                self.race_menu.set(self.last_selected_race)
 
         self.segmented_button_callback(self.segmented_buttons.get())
 
@@ -166,10 +169,16 @@ class MainApplication(ctk.CTk):
         self.countries = {}
         self.data.execute(f"SELECT track, date FROM races WHERE season = {season} ORDER BY round")
 
+        # Clear the race_values list
+        self.race_values.clear()
+
         for i in self.data:
             self.countries[i[1]] = i[0]
+            # Append race values to the race_values list
+            self.race_values.append(i[0])
 
-        self.race_menu.configure(values=list(self.countries.values()))
+        # Configure the race menu with the race_values list
+        self.race_menu.configure(values=self.race_values)
 
 
 def main():
