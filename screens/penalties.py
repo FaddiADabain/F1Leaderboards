e9c17dfe43dace50ba825583673a3b289718049a
Faddi Azzam Dabain <96977807+FaddiADabain@users.noreply.github.com>
2023-12-08 16:16:25 -0500

    Update penalties.py

--- screens/penalties.py
+++ screens/penalties.py
@@ -1,12 +1,12 @@
-import customtkinter as ctk  # Import CustomTkinter for enhanced tkinter widgets
-from datetime import date  # Import date class from datetime module for handling dates
+import customtkinter as ctk  # Enhanced tkinter module for improved UI elements
+from datetime import date  # Date module for handling date operations
 
-# Define a class 'Penalties' that inherits from CTkFrame, a customtkinter class for frames
+
+# Define the 'Penalties' class, inheriting from customtkinter's CTkFrame
 class Penalties(ctk.CTkFrame):
-    # Constructor for the Penalties class
     def __init__(self, parent, controller, db):
-        super().__init__(parent)  # Call the constructor of the base class
-        # Initialize various attributes to store penalties data and UI elements
+        super().__init__(parent)    # Initialize the parent class (CTkFrame)
+        # Initialize attributes for storing various data and UI elements
         self.scrollable_frame = None
         self.lapsL = None
         self.trackL = None
@@ -15,78 +15,102 @@ class Penalties(ctk.CTkFrame):
         self.lapsT = None
         self.trackT = None
         self.countryT = None
-        self.controller = controller  # Store the reference to the controller
-        self.data = db  # Store the database connection
-        self.load_data(self.controller.race_menu.get())  # Load the data based on the selected race
-        self.setup_ui()  # Call the method to setup the user interface
+        
+        self.controller = controller  # Controller reference
+        self.data = db  # Database connection
+        self.load_data(self.controller.race_menu.get())  # Load data based on the selected race
+        self.setup_ui()  # Setup the user interface
 
-    # Define a method to setup the user interface
     def setup_ui(self):
-        # Configure the grid layout for the frame
-        self.grid_rowconfigure([1, 2], weight=0)  # Configure rows for labels
-        self.grid_rowconfigure(3, weight=1)  # Configure row for scrollable frame
-        self.grid_columnconfigure([1, 2, 3], weight=1)  # Configure columns
+        # Set grid weights for layout management and create UI elements
+        # Configure rows and columns with appropriate weights for the layout
 
-        # Create and position UI elements: labels and scrollable frame
+        # Set weights for rows and columns
+        self.grid_rowconfigure(1, weight=0)  # Country label row
+        self.grid_rowconfigure(2, weight=0)  # Track row
+        self.grid_rowconfigure(3, weight=1)  # Scrollable Frame row
+
+        self.grid_columnconfigure(1, weight=1)  # Country and track label column
+        self.grid_columnconfigure(2, weight=1)  # This will be the container for the segmented button
+        self.grid_columnconfigure(3, weight=1)  # Laps and Pts label column
+
+        # UI Elements
+
         self.countryL = ctk.CTkLabel(self, text=self.countryT, font=("Lucidia Sans", 25))
-        self.trackL = ctk.CTkLabel(self, text=self.trackT, font=("Lucidia Sans", 20))
-        self.lapsL = ctk.CTkLabel(self, text=f"Laps: {self.lapsT}", font=("Lucidia Sans", 20))
-        self.scrollable_frame = ctk.CTkScrollableFrame(self, width=720, height=200)
-
-        # Position labels and scrollable frame using grid layout
         self.countryL.grid(row=1, column=1, sticky="nw", padx=20, pady=10)
+
+        self.trackL = ctk.CTkLabel(self, text=self.trackT, font=("Lucidia Sans", 20))
         self.trackL.grid(row=2, column=1, sticky="nw", padx=20)
+
+        self.lapsL = ctk.CTkLabel(self, text=f"Laps: {self.lapsT}", font=("Lucidia Sans", 20))
         self.lapsL.grid(row=1, column=2, sticky="ne", columnspan=3, padx=20, pady=10)
+
+        self.scrollable_frame = ctk.CTkScrollableFrame(self, width=720, height=200)
         self.scrollable_frame.grid(row=3, column=1, columnspan=3, pady=10, sticky="nsew")
 
-        self.fill_penalties()  # Call method to populate the penalties section
+        self.fill_penalties()
 
-    # Define a method to fill the penalties section with data
     def fill_penalties(self):
-        # Update labels with current data
+        # Update label text with the current data and populate penalties
+        # Clear existing content in the scrollable frame
+        # Iterate over fetched penalty data, creating and positioning labels
         self.countryL.configure(text=self.countryT)
         self.trackL.configure(text=self.trackT)
         self.lapsL.configure(text=f"Laps: {self.lapsT}")
 
-        # Clear existing widgets in scrollable frame before updating
         for widget in self.scrollable_frame.winfo_children():
             widget.destroy()
 
-        # Populate scrollable frame with penalties data
         i = 0
         while self.penalties:
-            nameT, sessionT, offenceT, decisionT = self.penalties  # Unpack data tuple
+            nameT = self.penalties[0]
+            sessionT = self.penalties[1]
+            offenceT = self.penalties[2]
+            decisionT = self.penalties[3]
 
-            # Create and position labels for each item in the data
-            nameL, sessionL, offenceL, decisionL = [ctk.CTkLabel(self.scrollable_frame, text=value, font=("Lucidia Sans", 17)) for value in [nameT, sessionT, offenceT, decisionT]]
+            self.scrollable_frame.grid_columnconfigure(0, weight=1)
+            self.scrollable_frame.grid_columnconfigure(1, weight=1)
+            self.scrollable_frame.grid_columnconfigure(2, weight=1)
+
+            nameL = ctk.CTkLabel(self.scrollable_frame, text=nameT, font=("Lucidia Sans", 17))
             nameL.grid(row=i, column=0, sticky="nw", padx=20, pady=10)
+
+            sessionL = ctk.CTkLabel(self.scrollable_frame, text=sessionT, font=("Lucidia Sans", 17))
             sessionL.grid(row=i, column=1, sticky="nw", padx=20, pady=10)
+
+            offenceL = ctk.CTkLabel(self.scrollable_frame, text=offenceT, font=("Lucidia Sans", 17))
             offenceL.grid(row=i, column=2, sticky="nw", padx=20, pady=10)
+
+            decisionL = ctk.CTkLabel(self.scrollable_frame, text=decisionT, font=("Lucidia Sans", 17))
             decisionL.grid(row=i, column=3, sticky="nw", padx=20, pady=10)
 
-            self.penalties = self.data.fetchone()  # Fetch next row of penalties
-            i += 1  # Increment row index
+            self.penalties = self.data.fetchone()
+            i += 1
 
-        # Define a method to load data for the penalties section
-        def load_data(self, race, season=date.today().year):
-            # SQL query to fetch race details
-            query = ("SELECT r.country, r.track, r.laps, r.season, r.round FROM races r WHERE r.season = ? AND r.track = ? ORDER BY date DESC")
-            self.data.execute(query, (season, race))  # Execute the query with provided season and race
-            fetched = self.data.fetchone()  # Fetch one record
+    def load_data(self, race, season=date.today().year):
+        # Load penalty data based on the selected race and season
+        # Execute SQL queries to fetch race details and penalty data
+        # Update class attributes with fetched data
+        query = (
+            f"SELECT r.country, r.track, r.laps, r.season, r.round FROM races r WHERE r.season = {season} AND r.track ="
+            "? ORDER BY date DESC")
+        self.data.execute(query, (race,))
+        fetched = self.data.fetchone()
 
-            # Extract and store race details
-            self.countryT = fetched[0]  # Country of the race
-            self.trackT = fetched[1]  # Track name
-            # Store the number of laps; if it's not updated yet, use a placeholder string
-            self.lapsT = str(int(fetched[2])) if fetched[2] is not None else "Not Updated Yet"
-            seasonT = fetched[3]  # Season year
-            roundT = fetched[4]  # Round number
+        self.countryT = fetched[0]
+        self.trackT = fetched[1]
+        if fetched[2] is not None:
+            self.lapsT = str(int(fetched[2]))
+        else:
+            self.lapsT = "Not Updated Yet"
+        seasonT = fetched[3]
+        roundT = fetched[4]
 
-            # SQL query to fetch penalties data
-            query = ("SELECT d.name, p.session, p.offence, p.decision "
-                     "FROM drivers d "
-                     "JOIN penalties p ON d.number = p.number AND d.season = p.season "
-                     f"WHERE p.round = {roundT} AND p.season = {seasonT} "
-                     "ORDER BY p.incident")
-            self.data.execute(query)  # Execute the query
-            self.penalties = self.data.fetchone()  # Fetch the first record of penalties
+        query = ("SELECT d.name, p.session, p.offence, p.decision "
+                 "FROM drivers d "
+                 "JOIN penalties p ON d.number = p.number AND d.season = p.season "
+                 f"WHERE p.round = {roundT} AND p.season = {seasonT} "
+                 "ORDER BY p.incident")
+        self.data.execute(query)
+
+        self.penalties = self.data.fetchone()
