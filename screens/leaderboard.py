16b97956e8562bce3131b56b78647e765437887e
Faddi Azzam Dabain <96977807+FaddiADabain@users.noreply.github.com>
2023-12-08 16:16:56 -0500

    Update leaderboard.py

--- screens/leaderboard.py
+++ screens/leaderboard.py
@@ -1,90 +1,106 @@
-import customtkinter as ctk  # Import CustomTkinter for enhanced tkinter widgets
-from datetime import date  # Import date class from datetime module for handling dates
+import customtkinter as ctk  # Enhanced tkinter module for improved UI elements
+from datetime import date  # Date module for handling date operations
 
-# Define a class 'Leaderboard' that inherits from CTkFrame, a customtkinter class for frames
+# Define the 'Leaderboard' class, inheriting from customtkinter's CTkFrame
 class Leaderboard(ctk.CTkFrame):
-    # Constructor for the Leaderboard class
     def __init__(self, parent, controller, db):
-        super().__init__(parent)  # Call the constructor of the base class
-        # Initialize various attributes to store leaderboard data and UI elements
-        self.lapsT = None
-        self.trackT = None
-        self.countryT = None
-        self.ptsL = None
-        self.lapsL = None
-        self.trackL = None
-        self.countryL = None
-        self.scrollable_frame = None
-        self.controller = controller  # Store the reference to the controller
-        self.data = db  # Store the database connection
-        self.setup_ui()  # Call the method to setup the user interface
+        super().__init__(parent)  # Initialize the parent class (CTkFrame)
+        # Initialize attributes for storing various data and UI elements
+        self.lapsT = None  # Variable to store laps
+        self.trackT = None  # Variable to store track information
+        self.countryT = None  # Variable to store country information
+        self.ptsL = None  # Label for points
+        self.lapsL = None  # Label for laps
+        self.trackL = None  # Label for track
+        self.countryL = None  # Label for country
+        self.scrollable_frame = None  # Scrollable frame for displaying leaderboard
+        self.controller = controller  # Controller reference
+        self.data = db  # Database connection
+        self.load_data(self.controller.race_menu.get())  # Load data based on the selected race
+        self.setup_ui()  # Setup the user interface
 
-    # Define a method to setup the user interface
     def setup_ui(self):
-        # Configure the grid layout for the frame
-        self.grid_rowconfigure([1, 2], weight=0)  # Configure rows for labels
-        self.grid_rowconfigure(3, weight=1)  # Configure row for scrollable frame
-        self.grid_columnconfigure([0, 1, 2], weight=1)  # Configure columns
+        # Set grid weights for layout management
+        self.grid_rowconfigure(1, weight=0)  # Weight for the country label row
+        self.grid_rowconfigure(2, weight=0)  # Weight for the track row
+        self.grid_rowconfigure(3, weight=1)  # Weight for the scrollable frame row
 
-        # Create and position UI elements: labels and scrollable frame
+        self.grid_columnconfigure(0, weight=1)  # Weight for the first column
+        self.grid_columnconfigure(1, weight=1)  # Weight for the second column
+        self.grid_columnconfigure(2, weight=1)  # Weight for the third column
+
+        # Create and configure UI elements (labels, scrollable frame)
         self.countryL = ctk.CTkLabel(self, text=self.countryT, font=("Lucidia Sans", 25))
         self.trackL = ctk.CTkLabel(self, text=self.trackT, font=("Lucidia Sans", 20))
         self.lapsL = ctk.CTkLabel(self, text=f"Laps: {self.lapsT}", font=("Lucidia Sans", 20))
         self.ptsL = ctk.CTkLabel(self, text="Pts.", font=("Lucidia Sans", 20))
         self.scrollable_frame = ctk.CTkScrollableFrame(self, width=720, height=200)
 
-        # Position labels and scrollable frame using grid layout
+        # Position the UI elements in the grid
         self.countryL.grid(row=1, column=0, sticky="nw", padx=20, pady=10)
         self.trackL.grid(row=2, column=0, sticky="nw", padx=20)
         self.lapsL.grid(row=1, column=1, sticky="ne", columnspan=3, padx=20, pady=10)
         self.ptsL.grid(row=2, column=1, sticky="ne", columnspan=3, padx=20)
         self.scrollable_frame.grid(row=3, column=0, columnspan=3, pady=10, sticky="nsew")
 
-        self.fill_leader()  # Call method to populate leaderboard
+        self.fill_leader()  # Populate the leaderboard data
 
-    # Define a method to fill the leaderboard with data
     def fill_leader(self):
-        # Update labels with current data
+        # Update label text with the current data
         self.countryL.configure(text=self.countryT)
         self.trackL.configure(text=self.trackT)
         self.lapsL.configure(text=f"Laps: {self.lapsT}")
 
-        # Clear existing widgets in scrollable frame before updating
+        # Clear existing content in the scrollable frame
         for widget in self.scrollable_frame.winfo_children():
             widget.destroy()
 
-        # Populate scrollable frame with leaderboard data
+        # Populate the leaderboard with fetched data
         for index, item in enumerate(self.data):
-            resultT, leaderT, ptsT = item  # Unpack data tuple
-            # Format points value
+            resultT = item[0]  # Result text
+            leaderT = item[1]  # Leader text
+            ptsT = item[2]  # Points text
+
+            # Format points as integer if possible
             ptsT_formatted = str(int(ptsT)) if ptsT.is_integer() else str(ptsT)
-            # Create and position labels for each item in the data
-            resultL, leaderL, ptsL = [ctk.CTkLabel(self.scrollable_frame, text=value, font=("Lucidia Sans", 17)) for value in [resultT, leaderT, ptsT_formatted]]
+
+            # Configure columns in the scrollable frame
+            self.scrollable_frame.grid_columnconfigure(0, weight=1)
+            self.scrollable_frame.grid_columnconfigure(1, weight=1)
+            self.scrollable_frame.grid_columnconfigure(2, weight=1)
+
+            # Create and position labels for result, leader, and points
+            resultL = ctk.CTkLabel(self.scrollable_frame, text=resultT, font=("Lucidia Sans", 17))
+            leaderL = ctk.CTkLabel(self.scrollable_frame, text=leaderT, font=("Lucidia Sans", 17))
+            ptsL = ctk.CTkLabel(self.scrollable_frame, text=ptsT_formatted, font=("Lucidia Sans", 17))
+            
+            # Grid placement for the result, leader, and points labels in the scrollable frame
             resultL.grid(row=index, column=0, sticky="nw", padx=20, pady=10)
             leaderL.grid(row=index, column=1, sticky="nw", padx=20, pady=10)
             ptsL.grid(row=index, column=2, sticky="e", padx=20, pady=10)
 
-    # Define a method to load data for the leaderboard
-        def load_data(self, race, season=date.today().year):
-            # SQL query to fetch race details
-            query = ("SELECT r.country, r.track, r.laps, r.season, r.round FROM races r WHERE r.season = ? AND r.track = ? ORDER BY date DESC")
-            self.data.execute(query, (season, race))  # Execute the query with provided season and race
-            fetched = self.data.fetchone()  # Fetch one record
+    def load_data(self, race, season=date.today().year):
+        # Load leaderboard data based on the selected race and season
+        # SQL query to fetch race details
+        query = ("SELECT r.country, r.track, r.laps, r.season, r.round FROM races r "
+                f"WHERE r.season = {season} AND r.track = ? ORDER BY date DESC")
+        self.data.execute(query, (race, ))  # Execute the query with the selected race
+        fetched = self.data.fetchone()  # Fetch the first record from the query result
 
-            # Extract and store race details
-            self.countryT = fetched[0]  # Country of the race
-            self.trackT = fetched[1]  # Track name
-            # Store the number of laps; if it's not updated yet, use a placeholder string
-            self.lapsT = str(int(fetched[2])) if fetched[2] is not None else "Not Updated Yet"
-            seasonT = fetched[3]  # Season year
-            roundT = fetched[4]  # Round number
+        # Update class attributes with the fetched race details
+        self.countryT = fetched[0]  # Country name
+        self.trackT = fetched[1]  # Track name
+        # Laps count, check if it is available; otherwise, use placeholder text
+        self.lapsT = str(int(fetched[2])) if fetched[2] is not None else "Not Updated Yet"
+        seasonT = fetched[3]  # Season year
+        roundT = fetched[4]  # Round number
 
-            # SQL query to fetch leaderboard data
-            query = ("SELECT r.status, d.name, r.points "
-                     "FROM results r "
-                     "JOIN drivers d ON r.number = d.number AND r.season = d.season "
-                     "JOIN races ra ON r.season = ra.season AND r.round = ra.round "
-                     f"WHERE r.season = {seasonT} AND r.round = {roundT} "
-                     "ORDER BY r.season, r.round, r.result")
-            self.data.execute(query)  # Execute the query
-
+        # SQL query to fetch leaderboard data for the given race and season
+        query = (
+            "SELECT r.status, d.name, r.points "
+            "FROM results r "
+            "JOIN drivers d ON r.number = d.number AND r.season = d.season "
+            "JOIN races ra ON r.season = ra.season AND r.round = ra.round "
+            f"WHERE r.season = {seasonT} AND r.round = {roundT} "
+            "ORDER BY r.season, r.round, r.result")
+        self.data.execute(query)  # Execute the query for leaderboard data
