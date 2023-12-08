295bd5392c01e21a091ffbe48eaa8025f7d5a3c0
Faddi Azzam Dabain <96977807+FaddiADabain@users.noreply.github.com>
2023-12-08 16:16:13 -0500

    Update standings.py

--- screens/standings.py
+++ screens/standings.py
@@ -1,60 +1,61 @@
-import customtkinter as ctk  # Import CustomTkinter for enhanced tkinter widgets
-import matplotlib.pyplot as plt  # Import matplotlib for plotting graphs
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg  # Import TkAgg backend for embedding matplotlib in tkinter
-from datetime import date  # Import date class from datetime module for handling dates
+import customtkinter as ctk  # Import the customtkinter library for advanced GUI elements
+import matplotlib.pyplot as plt  # Import matplotlib for plotting
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg  # Backend for embedding matplotlib in tkinter GUI
+from datetime import date  # Import date for handling date-related operations
 
-# Define a class 'Standings' that inherits from CTkFrame, a customtkinter class for frames
+# Define the 'Standings' class, inheriting from customtkinter's CTkFrame
 class Standings(ctk.CTkFrame):
-    # Constructor for the Standings class
+    # Constructor for the 'Standings' class
     def __init__(self, parent, controller, db):
-        super().__init__(parent)  # Call the constructor of the base class
-        self.scrollable_frame = None  # Initialize a variable for the scrollable frame
+        super().__init__(parent)  # Initialize the parent class (CTkFrame)
+        self.scrollable_frame = None  # Attribute for the scrollable frame
         self.controller = controller  # Store the reference to the controller
-        self.data = db  # Store the database connection
+        self.data = db  # Database connection
         self.table = "Drivers' Championship"  # Default table for standings
-        self.load_data(self.table)  # Load the data from the database
-        self.setup_ui()  # Call the method to setup the user interface
+        self.load_data(self.table)  # Load data for the default table
+        self.setup_ui()  # Set up the user interface
 
-    # Define a method to setup the user interface
+    # Method to set up the user interface
     def setup_ui(self):
-        # Configure the grid layout for the frame
-        self.grid_rowconfigure(3, weight=1)  # Configure row for scrollable frame
-        self.grid_columnconfigure(0, weight=1)  # Configure column
+        # Configure grid layout for the frame
+        self.grid_rowconfigure(3, weight=1)  # Assign weight to row for scaling
+        self.grid_columnconfigure(0, weight=1)  # Assign weight to column for scaling
 
-        # Create and position the scrollable frame
+        # Create a scrollable frame and add it to the grid
         self.scrollable_frame = ctk.CTkScrollableFrame(self, width=720, height=200)
         self.scrollable_frame.grid(row=3, column=0, columnspan=3, pady=10, sticky="nsew")
 
-        # Set up columns for the scrollable frame
+        # Configure columns inside the scrollable frame for layout management
         self.scrollable_frame.grid_columnconfigure(0, weight=1)
         self.scrollable_frame.grid_columnconfigure(1, weight=1)
         self.scrollable_frame.grid_columnconfigure(2, weight=1)
 
-    # Define a method to load data from the database
+    # Method to load data from the database
     def load_data(self, table="Drivers' Championship", season=date.today().year):
-        # Choose the query based on the selected table
+        # Fetch data based on the selected table (Drivers' or Constructors' Championship)
         if table == "Drivers' Championship":
             query = f"SELECT name, points, team FROM drivers WHERE season = {season} ORDER BY points DESC"
         elif table == "Constructors' Championship":
             query = f"SELECT name, points FROM teams WHERE season = {season} ORDER BY points DESC"
         else:
-            print("Error in table selected")
+            print("Error in table selected")  # Print an error if the table is not recognized
 
-        self.data.execute(query)  # Execute the query
-        self.table = table  # Update the table attribute
+        self.data.execute(query)  # Execute the database query
+        self.table = table  # Update the current table
 
-    # Define a method to populate the standings
+    # Method to fill the frame with fetched data
     def fill(self):
-        # Clear existing widgets in scrollable frame before updating
+        # Clear existing widgets in the scrollable frame
         for widget in self.scrollable_frame.winfo_children():
             widget.destroy()
 
-        fetched = self.data.fetchall()  # Fetch data from the database
+        fetched = self.data.fetchall()  # Fetch the data from the database
 
-        # Initialize lists for the pie chart data
-        labels = []
-        sizes = []
-        team_colors_list = []
+        # Initialize lists to store data for the pie chart
+        labels = []  # Labels for pie chart segments
+        sizes = []  # Sizes of pie chart segments
+        team_colors_list = []  # Colors for pie chart segments
+        # Dictionary to map team names to colors
         colors = {
             'Red Bull': '#1E41FF',
             'Mercedes': '#00D2BE',
@@ -68,44 +69,59 @@ class Standings(ctk.CTkFrame):
             'AlphaTauri': '#2B4562'
         }  # Define colors for teams
 
-        # Fetch data and fill the labels and sizes lists for the pie chart
+        # Process the fetched data to populate labels, sizes, and colors
         if self.table == "Drivers' Championship":
-            # Process data for drivers' championship
             for name, points, team in fetched:
                 labels.append(name)
-                sizes.append(points)
-                team_colors_list.append(colors.get(team, 'gray'))  # Use gray as default color
+                sizes.append(points)    
+                team_colors_list.append(colors.get(team))
         else:
-            # Process data for constructors' championship
             for name, points in fetched:
                 labels.append(name)
                 sizes.append(points)
-                team_colors_list.append(colors.get(name, 'gray'))  # Use gray as default color
+                team_colors_list.append(colors.get(name))
 
-        # Create a figure for the pie chart
+        # Create a matplotlib figure for the pie chart
         fig, ax = plt.subplots()
-        ax.pie(sizes, labels=labels, autopct='%1.1f%%', colors=team_colors_list)
+        # Set figure and axis background color
+        background_color = "#2e2e2e"
+        fig.patch.set_facecolor(background_color)
+        ax.set_facecolor(background_color)
+        # Create the pie chart with the collected data
+        # Create the pie chart and store the text and autotext elements
+        wedges, texts, autotexts = ax.pie(sizes, labels=labels, autopct='%1.1f%%', colors=team_colors_list)
 
-        # Set figure and axes properties for better visual appearance
-        fig.patch.set_facecolor('gray17')
-        ax.set_facecolor('gray17')
+        # Set the color of the labels to white
+        for text in texts:
+            text.set_color('white')
 
-        # Create a canvas and add the figure to it
+        # Embed the matplotlib figure in the tkinter frame
         canvas = FigureCanvasTkAgg(fig, master=self.scrollable_frame)
         canvas_widget = canvas.get_tk_widget()
         canvas_widget.grid(row=0, column=0, columnspan=3)
 
-        # Populate the scrollable frame with the standings data
-        for index, (name, points) in enumerate(fetched):
-            # Format the points value
-            pts_formatted = str(int(points)) if isinstance(points, float) and points.is_integer() else str(points)
+        # Add additional widgets below the pie chart for detailed standings
+        for index, item in enumerate(fetched):
+            # Process points data for display
+            leaderT = item[0]
+            ptsT = item[1]
+            ptsT_formatted = str
+            
+            # Process points data for display
+            leaderT = item[0]  # Name of the leader/driver or team
+            ptsT = item[1]  # Points achieved
 
-            # Create labels for rank, driver/team name, and points
-            rank_label = ctk.CTkLabel(self.scrollable_frame, text=index + 1, font=("Lucidia Sans", 17))
-            rank_label.grid(row=index + 1, column=0, sticky="nw", padx=20, pady=10)
+            # Check if points (ptsT) is an integer value and format it accordingly
+            ptsT_formatted = str(int(ptsT)) if ptsT.is_integer() else str(ptsT)
 
-            name_label = ctk.CTkLabel(self.scrollable_frame, text=name, font=("Lucidia Sans", 17))
-            name_label.grid(row=index + 1, column=1, sticky="nw", padx=20, pady=10)
+            # Create and position a label for the ranking/order number
+            resultL = ctk.CTkLabel(self.scrollable_frame, text=index + 1, font=("Lucidia Sans", 17))
+            resultL.grid(row=index + 1, column=0, sticky="nw", padx=20, pady=10)
 
-            points_label = ctk.CTkLabel(self.scrollable_frame, text=pts_formatted, font=("Lucidia Sans", 17))
-            points_label.grid(row=index + 1, column=2, sticky="e", padx=20, pady=10)
+            # Create and position a label for the leader/driver or team name
+            leaderL = ctk.CTkLabel(self.scrollable_frame, text=leaderT, font=("Lucidia Sans", 17))
+            leaderL.grid(row=index + 1, column=1, sticky="nw", padx=20, pady=10)
+
+            # Create and position a label for the points
+            ptsL = ctk.CTkLabel(self.scrollable_frame, text=ptsT_formatted, font=("Lucidia Sans", 17))
+            ptsL.grid(row=index + 1, column=2, sticky="e", padx=20, pady=10)
